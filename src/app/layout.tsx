e2314e8b40ebcdc3b638f03902655b74fb7d6776
Reynaldo Navedo <rnavedo@ariselms.com>
2025-04-21 22:55:38 -0400

    integrated flowbite started

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { ThemeModeScript } from "flowbite-react";
 import { Geist, Geist_Mono } from "next/font/google";
 import type { Metadata } from "next";
 import { LanguageContextProvider } from "@/context/languageContext";
@@ -30,28 +31,32 @@ export default function RootLayout({
 }>) {
   return (
 		<>
-			<Head>
-				<link rel="icon" href="/favicon.ico" />
-				<link
-					rel="apple-touch-icon"
-					sizes="180x180"
-					href="/apple-touch-icon.png"
-				/>
-				<link
-					rel="icon"
-					type="image/png"
-					sizes="32x32"
-					href="/favicon-32x32.png"
-				/>
-				<link
-					rel="icon"
-					type="image/png"
-					sizes="16x16"
-					href="/favicon-16x16.png"
-				/>
-				<link rel="manifest" href="/site.webmanifest" />
-			</Head>
-			<html lang="en" className="text-gray-50 relative">
+			<html
+				lang="en"
+				className="text-gray-50 relative"
+				suppressHydrationWarning>
+				<head>
+					<ThemeModeScript />
+					<link rel="icon" href="/favicon.ico" />
+					<link
+						rel="apple-touch-icon"
+						sizes="180x180"
+						href="/apple-touch-icon.png"
+					/>
+					<link
+						rel="icon"
+						type="image/png"
+						sizes="32x32"
+						href="/favicon-32x32.png"
+					/>
+					<link
+						rel="icon"
+						type="image/png"
+						sizes="16x16"
+						href="/favicon-16x16.png"
+					/>
+					<link rel="manifest" href="/site.webmanifest" />
+				</head>
 				<body
 					className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
 					{/* <img
