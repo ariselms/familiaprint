cac8dcddf39dea30b2e6fdf5c5614cd849c540ac
Reynaldo Navedo <rnavedo@ariselms.com>
2025-05-20 16:21:33 -0400

    fixing issue in prod

--- src/components/Materials.tsx
+++ src/components/Materials.tsx
@@ -117,7 +117,7 @@ const ServicesMaterialsList = () => {
 				<ServiceMaterialsHeader />
 				<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
 					{materials?.map((material: MaterialsType) => (
-							<ServicesMaterialsCard key={material.id} material={material} />
+							<ServicesMaterialsCard key={material?.id} material={material} />
 					))}
 				</div>
 			</MainFull>
